608b12ad12ced70973b5d5d35cd77c23b1c1cbb8
Benitosb <167676212+Benitosb@users.noreply.github.com>
2024-06-22 15:06:51 +0200

    dialog1,2,3 changes

--- Campus Quest/Assets/MovementAnimation/followplayer.cs
+++ Campus Quest/Assets/MovementAnimation/followplayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class followplayer : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 3f, -10f);
+    private Vector3 offset = new Vector3(0f, 3.8f, -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
